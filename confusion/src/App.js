d15b73660d0db616a157b0e6b58750db8cf0d878
aeskafi <arham.eskafi@gmail.com>
2022-01-20 13:02:15 +0300

    Components Part 1

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
+import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
