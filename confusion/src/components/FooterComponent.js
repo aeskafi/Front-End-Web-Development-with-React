a3973f76115c533ac10f3effc083cf9cf6b26989
aeskafi <arham.eskafi@gmail.com>
2022-01-21 15:18:24 +0300

    React Router

--- confusion/src/components/FooterComponent.js
+++ confusion/src/components/FooterComponent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function Footer ( props ) {
     return (
@@ -8,10 +9,10 @@ function Footer ( props ) {
                     <div className="col-4 offset-1 col-sm-2">
                         <h5>Links</h5>
                         <ul className="list-unstyled">
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Menu</a></li>
-                            <li><a href="contactus.html">Contact</a></li>
+                            <li><Link to="/home">Home</Link></li>
+                            <li><Link to="/aboutus">About</Link></li>
+                            <li><Link to="/menu">Menu</Link></li>
+                            <li><Link to="/contactus">Contact</Link></li>
                         </ul>
                     </div>
                     <div className="col-7 col-sm-5">
@@ -22,18 +23,18 @@ function Footer ( props ) {
                             HONG KONG<br />
                             <i className="fa fa-phone fa-lg"></i>: +852 1234 5678<br />
                             <i className="fa fa-fax fa-lg"></i>: +852 8765 4321<br />
-                            <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:confusion@food.net">
-                                confusion@food.net</a>
+                            <i className="fa fa-envelope fa-lg"></i>: <Link to="mailto:confusion@food.net">
+                                confusion@food.net</Link>
                         </address>
                     </div>
                     <div className="col-12 col-sm-4 align-self-center">
                         <div className="text-center">
-                            <a className="btn btn-social-icon btn-google" href="http://google.com/+"><i className="fa fa-google-plus"></i></a>
-                            <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/profile.php?id="><i className="fa fa-facebook"></i></a>
-                            <a className="btn btn-social-icon btn-linkedin" href="http://www.linkedin.com/in/"><i className="fa fa-linkedin"></i></a>
-                            <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-twitter"></i></a>
-                            <a className="btn btn-social-icon btn-google" href="http://youtube.com/"><i className="fa fa-youtube"></i></a>
-                            <a className="btn btn-social-icon" href="mailto:"><i className="fa fa-envelope-o"></i></a>
+                            <Link className="btn btn-social-icon btn-google" to="http://google.com/+"><i className="fa fa-google-plus"></i></Link>
+                            <Link className="btn btn-social-icon btn-facebook" to="http://www.facebook.com/profile.php?id="><i className="fa fa-facebook"></i></Link>
+                            <Link className="btn btn-social-icon btn-linkedin" to="http://www.linkedin.com/in/"><i className="fa fa-linkedin"></i></Link>
+                            <Link className="btn btn-social-icon btn-twitter" to="http://twitter.com/"><i className="fa fa-twitter"></i></Link>
+                            <Link className="btn btn-social-icon btn-google" to="http://youtube.com/"><i className="fa fa-youtube"></i></Link>
+                            <Link className="btn btn-social-icon" to="mailto:"><i className="fa fa-envelope-o"></i></Link>
                         </div>
                     </div>
                 </div>
--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
 import {DISHES} from '../shared/dishes';
 import Footer from './FooterComponent';
-
+import {Switch, Route, Redirect} from 'react-router-dom';
 class Main extends Component {
 
     constructor( props ) {
@@ -16,18 +17,21 @@ class Main extends Component {
         }
     }
 
-    onDishSelect ( dishId ) {
-        this.setState( {selectedDish: dishId} );
-    }
-
     render () {
+        const HomePage = () => {
+            return (
+                <Home />
+            )
+        }
+
         return (
             <div>
                 <Header />
-                <Menu dishes={this.state.dishes}
-                    onClick={( dishId ) => this.onDishSelect( dishId )} />
-                <DishDetail
-                    dish={this.state.dishes.filter( ( dish ) => dish.id === this.state.selectedDish )[0]} />
+                <Switch>
+                    <Route path="/home" component={HomePage} />
+                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                    <Redirect to="/home" />
+                </Switch>
                 <Footer />
             </div>
         );
