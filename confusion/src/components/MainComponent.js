08aaf98269f6db773c9419f6da1cf781fa7995e7
aeskafi <arham.eskafi@gmail.com>
2022-01-25 12:25:02 +0300

    remove useless constructor

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -33,10 +33,6 @@ const mapDispatchToProps = ( dispatch ) => ( {
 
 class Main extends Component {
 
-    constructor( props ) {
-        super( props );
-    }
-
     componentDidMount () {
         this.props.fetchDishes();
         this.props.fetchComments();
